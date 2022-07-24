dc181c8594e81b859c8ab67bdbf9fb35df664d47
UntamedLotus <ruttikajadhav@gmail.com>
2022-07-24 19:53:29 +0530

    Initial commit

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+export function Header() {
+  return (
+    <div className="App">
+      <img
+        className="profile-img"
+        src="/images/profile.jpg"
+        alt="user_profile"
+      />
+    </div>
+  );
+}
